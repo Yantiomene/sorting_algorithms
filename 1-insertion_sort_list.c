d7ba3e89188850bcffcecbb8b225c38c7916da4f
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-06-22 14:39:59 -0700

    [Fix] Fixing the case of a NULL list pointer

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,7 +10,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *ptr, *curr, *left_top;
 
-	if (!*list || !(*list)->next)
+	if (!list || !*list || !(*list)->next)
 		return;
 	ptr = (*list)->next;
 	while (ptr)
